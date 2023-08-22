374d26e0413815337f3f2eb8d9d402879db94ece
Humberto Lopez <101685223+Betunx@users.noreply.github.com>
2023-08-21 21:54:39 -0700

    Update buttons.js

--- buttons.js
+++ buttons.js
@@ -1,10 +1,17 @@
 // HTML reference
 
 const btnfeatures = document.getElementById("btnfeatures");
-const inputlink = document.querySelector("inputlink");
+const spanLink = document.getElementById("inputlink");
+
+
+
 
 // Events
 btnfeatures.addEventListener("click", () => {
-alert("You clicked the botton!!");
+  alert("You clicked the botton!!");
+});
 
+
+spanLink.addEventListener("click", () => {
+  spanLink.textContent= "";
 });
