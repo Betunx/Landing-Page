3fd12c2ec9c1d6ffad78062d9539ab0e92e8412b
Humberto Lopez <101685223+Betunx@users.noreply.github.com>
2023-08-22 16:17:35 -0700

    Update buttons.js

--- buttons.js
+++ buttons.js
@@ -3,15 +3,27 @@
 const btnfeatures = document.getElementById("btnfeatures");
 const spanLink = document.getElementById("inputlink");
 
-
-
-
 // Events
 btnfeatures.addEventListener("click", () => {
   alert("You clicked the botton!!");
 });
 
-
 spanLink.addEventListener("click", () => {
-  spanLink.textContent= "";
+  // updateText();
+  // toggleCursor();
+  // setInterval(toggleCursor, 500);
+  spanLink.textContent = " ";
 });
+
+//funciones
+
+// function updateText() {
+//   if (cursorVisible) {
+//     spanLink.textContent = " " + "|"; // Agregar el cursor
+//   }
+// }
+
+// function toggleCursor() {
+//   cursorVisible = !cursorVisible;
+//   updateText();
+// }
