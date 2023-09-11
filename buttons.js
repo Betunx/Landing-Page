ee766a15e03d3204872a7f782b48991ccae16ae1
Humberto Lopez <101685223+Betunx@users.noreply.github.com>
2023-09-11 16:09:03 -0700

    Update buttons.js

--- buttons.js
+++ buttons.js
@@ -5,7 +5,8 @@ const spanLink = document.getElementById("inputlink");
 const headerr = document.getElementById("headerid");
 const navi = document.querySelector("header img");
 const navigation = document.getElementById("nav");
-const contenedor= document.getElementsByClassName('contenedormain');
+const navguerri = document.getElementsByClassName("navimg");
+const contenedor = document.getElementsByClassName("contenedormain");
 let menu = document.createElement("div");
 let features1 = document.createElement("span");
 let pricing1 = document.createElement("span");
@@ -13,22 +14,29 @@ let resources1 = document.createElement("span");
 let login1 = document.createElement("span");
 let signUp1 = document.createElement("span");
 let line = document.createElement("span");
-
+let imgwork = document.createElement("img");
 //Events
+
+imgwork.src = "./Assets/imgs/illustration-working.svg";
+imgwork.className = "workimgmobile";
+document.body.appendChild(imgwork);
+document.body.insertBefore(imgwork, document.body.childNodes[2]);
 
 
 spanLink.addEventListener("click", () => {
   spanLink.textContent = " ";
 });
 
-navigation.addEventListener("click", () => {
+
+navigation.addEventListener("mouseenter", () => {
+  document.body.removeChild(imgwork);
   menu.className = "menuid";
   document.body.appendChild(menu);
   document.body.insertBefore(menu, document.body.childNodes[2]);
   features1.innerText = "Features";
   pricing1.innerText = "Princing";
   resources1.innerText = "Resources";
-  resources1.className = 'resnav';
+  resources1.className = "resnav";
   line.innerText = "";
   login1.innerText = "Login";
   signUp1.innerText = "Sign Up";
@@ -42,6 +50,8 @@ navigation.addEventListener("click", () => {
   menu.appendChild(signUp1);
 });
 
-navigation.addEventListener("mouseleave", () => {
+navigation.addEventListener("mouseout", () => {
   document.body.removeChild(menu);
+  document.body.appendChild(imgwork);
+  document.body.insertBefore(imgwork, document.body.childNodes[2]);
 });
